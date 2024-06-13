006e637035761510f3d7aaa68b182999e1980d80
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-06-13 20:10:36 +0200

    Added: fix layout + AddTask / CancelTask functions + fontawesome

--- main.js
+++ main.js
@@ -4,12 +4,32 @@ createApp({
   data() {
     return {
         tasks: [],
+        userNewTask:"",
     }
   },
   methods: {
     setCompleted(task){
         task.completed = !task.completed;
+    },
+
+    addNewTask(){
+      let userTask = [
+        {
+          name: "",
+          completed: false,
+        }
+      ]
+      userTask.name = this.userNewTask;
+
+      this.tasks.push(userTask);
+      this.userNewTask = "";
+    },
+
+    cancelTask(index){
+      this.tasks.splice(index,1);
     }
+
+
   },
   mounted() {
     axios.get("./server/server.php").then(results => {
