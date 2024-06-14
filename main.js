0a4c04d966c074d30742508df326cc6401e38642
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-06-14 17:14:59 +0200

    Added: addTask in JSON

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ createApp({
     return {
         tasks: [],
         userNewTask:"",
+
+        apiUrl: "./server/server.php",
+        postRequestConfig: {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        },
     }
   },
   methods: {
@@ -13,28 +20,39 @@ createApp({
     },
 
     addNewTask(){
-      let userTask = [
+      const userTask = [
         {
-          name: "",
-          completed: false,
+          name: this.userNewTask,
+          completed: false
         }
       ]
-      userTask.name = this.userNewTask;
 
-      this.tasks.push(userTask);
+      axios.post(this.apiUrl, userTask, this.postRequestConfig).then(results => {
+        console.log("Risultati: ", results.data);
+        this.tasks = results.data;
+      });
+
+
+
       this.userNewTask = "";
     },
 
     cancelTask(index){
       this.tasks.splice(index,1);
-    }
+    },
+
+
+
+    getTaskList(){
+      axios.get("./server/server.php").then(results => {
+        console.log("Risultati: ", results);
+        this.tasks = results.data;
+      });
+    },
 
 
   },
   mounted() {
-    axios.get("./server/server.php").then(results => {
-        console.log("Risultati: ", results);
-        this.tasks = results.data;
-    });
+    this.getTaskList();
   }
 }).mount('#app')
--- server/server.php
+++ server/server.php
@@ -1,30 +1,34 @@
-<?php 
+<?php
 
-$tasks = [
-    [
-        'name' => 'Chiamare ufficio',
-        'completed' => false,
-    ],
-    [
-        'name' => 'Sistemare progetto',
-        'completed' => false,
-    ],
-    [
-        'name' => 'Chiamare cliente',
-        'completed' => false,
-    ],
-    [
-        'name' => 'Bug Fix',
-        'completed' => false,
-    ],
-    [
-        'name' => 'Riunione con il team',
-        'completed' => false,
-    ],
-];
+//leggo il file json dal disco
+$fileContent = file_get_contents("../dati.json");
+
+
+//se ho i dati per aggiungere un task...
+if( isset($_POST["name"]) && isset($_POST["completed"])) {
+
+    //converto il json in un array associativo php
+    $tasks = json_decode($fileContent, true);
+
+    //creo un nuovo task in php
+    $newTask = [
+        "name" => $_POST["name"],
+        "completed" => $_POST["completed"]
+    ];
+
+
+    //pusho il nuovo task nel mio array
+    $tasks[] = $newTask;
+
+    //converto tutto l'array in un json
+    $fileContent = json_encode($tasks);
+
+    //scrivo il json su disco
+    file_put_contents("../dati.json", $fileContent);
+    
+}
+
 
 header('Content-Type: application/json');
 
-$jsonString = json_encode($tasks);
-
-echo $jsonString;
+echo $fileContent;
